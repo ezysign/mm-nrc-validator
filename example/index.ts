44efc33660fb374b0bbbfec0997c30aea7c6b4b9
win <winhtaikaung28@gmail.com>
2020-10-06 10:14:35 +0630

    add some improvement on test

---
+++ example/index.ts
@@ -0,0 +1,7 @@
+import * as NRCValidator from '../dist'
+
+console.log(NRCValidator.verifyNRC('၁၂/မဂတ(နိုင်)၀၉၈၇၆၄'))
+console.log(NRCValidator.getMatchNRC('၁၂/မဂတ(နိုင်)၀၉၈၇၆၄'))
+console.log(NRCValidator.validateNRC('၁၂/မဂတ(နိုင်)၀၉၈၇၆၄'))
+console.log(NRCValidator.extractNrcInfo('၁၂/မဂတ(နိုင်)၀၉၈၇၆၄'))
+
--- src/test/nrc-verifier.test.ts
+++ src/test/nrc-verifier.test.ts
@@ -5,17 +5,18 @@ describe('Calculate Action', () => {
     expect(validateNRC('၁၂/မဂတ(နိုင်)၀၇၆၄')).toEqual(false)
   })
 
-  test('should returnNRCMatchArr  ', () => {
+  test('should returnNRCMatchArr', () => {
     expect(getMatchNRC('၁၂/မဂတ(နိုင်)၀၉၈၇၆၄')).toEqual([ '၁၂/မဂတ(နိုင်)၀၉၈၇၆၄', '၁၂', 'မဂတ', '(နိုင်)', '၀၉၈၇၆၄' ])
-    expect(getMatchNRC('၁၂/မဂတ(နိုင်)asdf')).toEqual([])
+    expect(()=>getMatchNRC('၁၂/မဂတ(နိုင်)asdf')).toThrowError("Invalid NRC Number")
   })
 
 
-  test('should verifyNRC  ', () => {
+  test('should verifyNRC', () => {
     expect(verifyNRC('၁၂/မဂတ(နိုင်)၀၉၈၇၆၄')).toEqual(true)
+    expect(verifyNRC('၁၂/မဝဝ(နိုင်)၀၉၈၇၆၄')).toEqual(false)
     
   }) 
-  test('should extract Info  ', () => {
+  test('should extract Info', () => {
     
     expect(extractNrcInfo('၁၂/မဂတ(နိုင်)၀၉၈၇၆၄')).toEqual({
         sr_code_en: '12',
@@ -27,6 +28,7 @@ describe('Calculate Action', () => {
       })
 
       expect(extractNrcInfo('၁၂/ကဘဘ(နိုင်)၀၉၈၇၆၄')).toEqual(undefined)
+      expect(extractNrcInfo('၁/ကဘဘ(နိုင်)၀၉၈၇၆၄')).toEqual(undefined)
     
   })
 
